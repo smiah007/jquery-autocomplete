76f995f501de910ca06bcf525fb93cbba2766bbc
Shaj Miah <shaj.k.miah@gmail.com>
2018-08-26 21:15:28 +0000

    Removed passwords from db controller

--- dbcontroller.php
+++ dbcontroller.php
@@ -2,7 +2,7 @@
 class DBController {
 	private $host = "localhost";
 	private $user = "root";
-	private $password = "woohoo";
+	private $password = "******";
 	private $database = "country";
 	private $conn;
 	
